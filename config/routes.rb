0503522aef6e93e8a0960fa1021d1a9c20f9739c
Gautam Gupta <ggupta78@gmail.com>
2022-11-09 13:07:09 +0800

    complete restaurant create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, except: %i[edit update destroy] do
+    resources :reviews, only: %i[new create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+indian_accent = { name: 'Indian Accent', address: '21, The Lodhi', phone_number: '+91 9999911111', category: 'chinese' }
+big_chill = { name: 'Big Chill', address: '31, Khan Market', phone_number: '+91 9999911112', category: 'italian' }
+have_more = { name: 'Have More', address: '41, Pandara Market', phone_number: '+91 9999911113', category: 'japanese' }
+moti_mahal = { name: 'Moti Mahal', address: '51, Daryaganj', phone_number: '+91 9999911114', category: 'french' }
+chez_gautam = { name: 'Chez Gautam', address: '61, Meherchand', phone_number: '+91 9999911115', category: 'belgian' }
+
+[indian_accent, big_chill, have_more, moti_mahal, chez_gautam].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
